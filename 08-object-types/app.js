b0b12bb1c2d0b97b2fa6f8fbc34568261a012361
odinPcWindows <odin.araujo.barragan@gmail.com>
2021-09-20 18:59:56 -0500

    Object types

---
+++ 08-object-types/app.js
@@ -0,0 +1,15 @@
+var person1 = {
+    name: 'Odin',
+    age: 5
+};
+var person2 = {
+    name: 'Odin',
+    age: 5
+};
+var person3 = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person1);
+console.log(person2);
+console.log(person3);
---
+++ 08-object-types/app.ts
@@ -0,0 +1,26 @@
+const person1 = {
+    name:'Odin',
+    age:5
+}
+
+const person2: object = {
+    name: 'Odin',
+    age: 5
+}
+
+const person3: {} = {
+    name:'Odin',
+    age:30
+};
+
+const person4:{
+    name:string,
+    age:number
+}={
+    name:'Maximilian',
+    age:38
+}
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
