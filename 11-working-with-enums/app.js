71f2cc9403d6f46223d77ca188808e7279a60ad8
odin-huawei <odin.araujo.barragan@gmail.com>
2021-09-23 06:51:49 -0500

    Enums

--- 11-working-with-enums/app.js
+++ 11-working-with-enums/app.js
@@ -1,1 +1,36 @@
 console.log('Working with enums');
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var Role2;
+(function (Role2) {
+    Role2[Role2["ADMIN"] = 0] = "ADMIN";
+    Role2[Role2["READ_ONLY"] = 1] = "READ_ONLY";
+    Role2[Role2["AUTHOR"] = 2] = "AUTHOR";
+})(Role2 || (Role2 = {}));
+;
+var Role3;
+(function (Role3) {
+    Role3["ADMIN"] = "ADMIN";
+    Role3["READ_ONLY"] = "READ_ONLY";
+    Role3["AUTHOR"] = "AUTHOR";
+})(Role3 || (Role3 = {}));
+;
+var Role4;
+(function (Role4) {
+    Role4["ADMIN"] = "ADMIN";
+    Role4[Role4["READ_ONLY"] = 1] = "READ_ONLY";
+    Role4["AUTHOR"] = "AUTHOR";
+})(Role4 || (Role4 = {}));
+; //Mixing types is valid
+var person = {
+    name: 'Odin',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+console.log(person);
--- 11-working-with-enums/app.ts
+++ 11-working-with-enums/app.ts
@@ -1,1 +1,16 @@
 console.log('Working with enums');
+
+enum Role{ADMIN,READ_ONLY,AUTHOR};
+enum Role2{ADMIN=0,READ_ONLY=1,AUTHOR=2};
+enum Role3{ADMIN='ADMIN',READ_ONLY='READ_ONLY',AUTHOR='AUTHOR'};
+enum Role4{ADMIN='ADMIN',READ_ONLY=1,AUTHOR='AUTHOR'};//Mixing types is valid
+
+const person ={
+    name:       'Odin',
+    age:        30,
+    hobbies:    ['Sports','Cooking'],
+    role :      Role.ADMIN
+}
+
+console.log(person);
+
